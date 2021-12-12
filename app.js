25f9c43d0285ac10cc15cc21bd332fade765317b
namsupark <skatn7979@gmail.com>
2021-12-12 17:52:48 +0900

    https

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require('express');
 const app = express();
 const userRouter = require('./routers/user');
 const cookieParser = require('cookie-parser');
+const https = require('https');
+const fs = require('fs');
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.text());
 
-app.use('/user',userRouter);
+app.use('/user', userRouter);
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Hellow World!!');
 });
 
-app.listen(80,() => console.log('localhost:5000 opened!!'));
+https.createServer(
+    {
+        key: fs.readFileSync(__dirname + '/key.pem', 'utf-8'),
+        cert: fs.readFileSync(__dirname + '/cert.pem', 'utf-8'),
+    },
+    app
+).listen(80);
+
+//app.listen(80, () => console.log('localhost:5000 opened!!'));
