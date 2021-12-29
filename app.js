fc09ae8d4c43a2c1e3c659352c04dc3b8c453c19
namsupark <skatn7979@gmail.com>
2021-12-29 20:42:04 +0900

    a

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userRouter = require('./routers/user');
-const cookieParser = require('cookie-parser');
-const https = require('https');
-const fs = require('fs');
+const cookieParser = require("cookie-parser");
+const https = require("https");
+const fs = require("fs");
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.text());
-
-app.use('/user', userRouter);
-
-app.get('/', (req, res) => {
-    res.send('Hellow World!!');
+app.get("/", (req, res) => {
+  res.send("Hellow World!!");
 });
 
-app.listen(80, () => console.log('localhost:5000 opened!!'));
+app.listen(80, () => console.log("localhost:80 opened!!"));
