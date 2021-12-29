c69d9dd1b044584c87bc43f065f1144a21f6a345
namsupark <skatn7979@gmail.com>
2021-12-29 22:26:46 +0900

    a

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/AWSTEST
-authbind --deep pm2 stop app.js
-rm -rf .gitignore
-rm -rf package.json
+#authbind --deep pm2 stop app.js
+#rm -rf .gitignore
+#rm -rf package.json
